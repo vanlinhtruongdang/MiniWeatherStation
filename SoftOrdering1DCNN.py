fa597186e0042f0a5aef9d226bd7bce617c6b415
MurazeKrinas <vanlinh.1942003@gmail.com>
2023-12-08 14:44:28 +0000

    Update

---
+++ SoftOrdering1DCNN.py
@@ -0,0 +1,144 @@
+import torch
+from torch import nn
+from torch.nn.utils.parametrizations import weight_norm
+from einops import rearrange
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class MLP(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout = 0.2):
+        super().__init__()
+        self.feedfoward = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout1d(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout1d(dropout)
+        )
+    def forward(self, x):
+        return self.feedfoward(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 4, dim_head = 16, dropout = 0.2):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout1d(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class SoftOrdering1DCNN(nn.Module):
+
+    def __init__(self, input_dim, output_dim, sign_size=16, cha_input=8, cha_hidden=32, dropout_rate = 0.3):
+        super().__init__()
+
+        hidden_size = sign_size*cha_input
+        attn_dim = cha_hidden
+        mlp_dim = attn_dim * 2
+        output_size = (sign_size//4) * cha_hidden
+        self.sign_size = sign_size
+        self.cha_input = cha_input
+
+        # Dropout
+        self.dropout = nn.Dropout1d(dropout_rate)
+
+        # Input
+        self.BnInput = nn.BatchNorm1d(input_dim)
+        InputDense = nn.Linear(input_dim, hidden_size, bias=False)
+        self.InputDense = weight_norm(InputDense)
+
+        # Pooling layer
+        self.AvgPooling = nn.AvgPool1d(kernel_size=4, stride=2, padding=1)
+        self.MaxPooling = nn.MaxPool1d(kernel_size=4, stride=2, padding=1)
+
+        # 1st conv layer
+        self.layer1 = self.make_layer(cha_input, cha_input*2, 5, 1, 2, groups=cha_input)
+
+        # 2nd conv layer
+        self.layer2 = self.make_layer(cha_input*2, cha_hidden, 3, 1, 1)
+
+        # 3rd conv layer
+        self.layer3 = self.make_layer(cha_hidden, cha_hidden, 5, 1, 2, groups=cha_hidden)
+
+        #Attention
+        self.attention = PreNorm(attn_dim, Attention(attn_dim))
+        self.mlp = PreNorm(attn_dim, MLP(attn_dim, mlp_dim))
+
+        # Output
+        self.flatten = nn.Flatten()
+        self.BnOutput = nn.BatchNorm1d(output_size)
+        OutputDense = nn.Linear(output_size, output_dim, bias=False)
+        self.OutputDense = weight_norm(OutputDense)
+
+    def make_layer(self, cha_input, cha_output, KernelSize, stride, padding, groups=1, dropout_rate=0.3):
+        conv = nn.Conv1d(
+            cha_input,
+            cha_output,
+            kernel_size = KernelSize,
+            stride = stride,
+            padding = padding,
+            groups = groups,
+            bias = False)
+
+        Layer = nn.Sequential(
+            weight_norm(conv, dim=None),
+            nn.BatchNorm1d(cha_output),
+            nn.CELU(),
+            nn.Dropout1d(dropout_rate),
+            )
+
+        return Layer
+
+    def forward(self, x):
+        x = self.BnInput(x)
+        x = nn.functional.celu(self.InputDense(x))
+        x = x.reshape(x.shape[0], self.cha_input, self.sign_size)
+
+        x = self.layer1(x)
+        x = self.MaxPooling(x)
+
+        x = self.layer2(x)
+        
+        x = x.permute(0,2,1)
+        x = self.attention(x) + x
+        x = self.mlp(x) + x
+        x = x.permute(0,2,1)
+
+        x = self.layer3(x)
+        x = self.AvgPooling(x)
+
+        x = self.flatten(x)
+        x = self.BnOutput(x)
+        x = self.OutputDense(x)
+        x = self.dropout(x)
+
+        return nn.functional.softmax(x, dim=-1)
---
+++ weather.py
@@ -0,0 +1,141 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import requests
+import pandas as pd
+from datetime import datetime, timezone, timedelta
+import schedule
+import time
+import os
+from SoftOrdering1DCNN import SoftOrdering1DCNN
+import torch
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+API_Key = "c8247668d0e5640ca269aa61eabfc4e3"
+Dataset = pd.DataFrame(columns=['datetime', 'lon', 'lat', 'temperature', 'wind_speed', 'humidity', 'cloud', 'pressure', 'visibility', 'rain', 'predict'])
+
+Latitude = 10.869778736885038
+Longtitude = 106.80280655508835
+Timestamp = None
+pipeline = None
+
+OpenweatherAPI = f"https://api.openweathermap.org/data/2.5/weather"
+Params = {
+        'lat': Latitude,
+        'lon': Longtitude,
+        'appid': API_Key,
+        'units': 'metric',
+    }
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+def PipelineInit():
+    global pipeline
+    from sklearn.preprocessing import StandardScaler, Normalizer
+    from sklearn.pipeline import Pipeline
+
+    TrainData = np.load('./Data.npy')
+    TrainLabel = np.load('./Target.npy')
+    pipeline = Pipeline([
+        ('Standard scaling', StandardScaler()),
+        ('Normalize', Normalizer())]).fit(X=TrainData, y=TrainLabel)
+    
+def ConvertUTCTimestamp(UTCTimestamp, getdatetime = False):
+    UTC_Datetime = datetime.utcfromtimestamp(UTCTimestamp)
+    HCM_Timezone = timezone(timedelta(hours=7))
+    VN_Datetime = UTC_Datetime.replace(tzinfo=timezone.utc).astimezone(HCM_Timezone)
+
+    if not getdatetime:
+        VN_Datetime = VN_Datetime.strftime('%Y')
+    else:
+        VN_Datetime = VN_Datetime.strftime('%H:%M:%S %d-%m-%Y')
+
+    return VN_Datetime
+
+def SaveDataset(Timestamp, isInterrupt = False):
+    global Dataset
+
+    Datetime = ConvertUTCTimestamp(Timestamp)
+    FileName = f'{Datetime}.csv'
+    SaveDirectory = f'/usr/share/grafana/public/WeatherData' if not isInterrupt else f'/usr/share/grafana/public/WeatherData/Interrupt'
+
+    FilePath = os.path.join(SaveDirectory, FileName)
+    Dataset.drop_duplicates(inplace=True)
+    Dataset.to_csv(FilePath, index=False)
+    prGreen(f"Dataset was saved successfully: {FilePath}")
+
+    if isInterrupt:
+        exit()
+
+
+def GetWeatherData():
+    global Dataset
+    global Timestamp
+
+    session = requests.session()
+    response = session.get(url=OpenweatherAPI, params=Params)
+
+    Model = SoftOrdering1DCNN(5, 2)
+    Model.load_state_dict(torch.load("./ModelCheckpoint/SoftOrdering1DCNN.pth"))
+    Model.eval()
+
+    if response.status_code == 200:
+        prGreen("Get weather data from OpenWeather successfully!")
+        ResponseData = response.json()
+        Timestamp = ResponseData['dt']
+
+        CityName = ResponseData['name']
+        lon = str(ResponseData['coord']['lon'])
+        lat = str(ResponseData['coord']['lat'])
+        WeatherState = str(ResponseData['weather'][0]['description'])
+
+        print(f'Location: {CityName} ({lon}, {lat})')
+        print(f'Weather state: {WeatherState}')
+
+        WeatherData = {
+            'datetime': ConvertUTCTimestamp(Timestamp, getdatetime=True),
+            'lon': lon,
+            'lat': lat,
+            'temperature': float(ResponseData['main']['temp']), # Celius
+            'wind_speed': float(ResponseData['wind']['speed']), # metter/s
+            'humidity': float(ResponseData['main']['humidity']), # Percentage
+            'cloud': float(ResponseData['clouds']['all']), # Percentage
+            'pressure': float(ResponseData['main']['pressure']), # hpa
+            'visibility': float(ResponseData['visibility']), # metter
+            'rain': float(ResponseData['rain']['1h']) if 'rain' in ResponseData else 0.0, # milimeter
+            'predict': None
+        }
+
+        Dataset = Dataset._append(WeatherData, ignore_index=True)
+
+        Features = ['temperature', 'wind_speed', 'humidity', 'cloud', 'pressure']
+        DataNumpy = Dataset[Features].iloc[-1].values
+        DataNumpy = pipeline.transform(DataNumpy.reshape(1,-1))
+        
+        Data = torch.from_numpy(DataNumpy).float().to(device)
+        Predict = Model(Data)
+
+        Result = torch.max(Predict).detach().numpy() * 100
+        Dataset.at[Dataset.index[-1], 'predict'] = Result
+
+        print(Dataset.iloc[-1])
+        SaveDataset(Timestamp)
+
+    else:
+        prRed("An error occurred while retrieving weather data from OpenWeather!")
+
+
+if __name__ == '__main__':
+    PipelineInit()
+    
+    schedule.every(interval=1).hours.do(GetWeatherData)
+
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        SaveDataset(Timestamp, isInterrupt=True)
